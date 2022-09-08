79435f92eda77544a625e9f79fd6bb6de0b3b484
Dantron98 <alandtl38@gmail.com>
2022-09-07 21:15:22 -0500

    Mide el timepo de ejecucion, se le probaron distintos valores de neuronas en la capa intermedia

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
 import mnist_loader
 import network
 import pickle
+import time
+start = time.time()
 training_data, validation_data , test_data = mnist_loader.load_data_wrapper()
 training_data = list(training_data)
 test_data = list(test_data)
-net =network.Network([784 ,30 ,10])
+net =network.Network([784, 20, 10])
 net.SGD( training_data, 30, 10, 3.0, test_data=test_data)
-archivo = open("red_prueba1.pkl" ,'wb')
-pickle.dump(net ,archivo)
+archivo = open("red_prueba1.pkl", 'wb')
+pickle.dump(net, archivo)
 archivo.close()
+print(time.time()-start)
 exit()
 # leer el archivo
-archivo_lectura = open("red_prueba.pkl" ,'rb')
+archivo_lectura = open("red_prueba.pkl", 'rb')
 net = pickle.load(archivo_lectura)
 archivo_lectura.close()
 net.SGD( training_data, 10, 50, 0.5, test_data=test_data)
-archivo = open("red_prueba.pkl" ,'wb')
-pickle.dump(net ,archivo)
+archivo = open("red_prueba.pkl", 'wb')
+pickle.dump(net, archivo)
 archivo.close()
+print(time.time()-start)
 exit()
